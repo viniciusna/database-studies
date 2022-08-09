a6c3202bb61432c37a1cdf5a67f68f306f4c09b7
Vinicius Noronha <viniciusna633@outlook.com>
2022-08-08 21:29:33 -0300

    session 4 finished of postgres course

---
+++ PostgreSQL/udemy-course/annotations/data-manipulation.sql
@@ -0,0 +1,21 @@
+-- Inserting data
+
+-- INSERT INTO [nome da tabela] ([coluna1], [coluna2], ...)
+-- VALUES ([valor da coluna 1], [valor da coluna 2], ...),
+-- ([valor da coluna 1], [valor da coluna 2], ...),
+-- ...;
+
+--#############################################################################################################################
+
+-- Updating an column
+
+-- UPDATE [nome da tabela]
+-- SET [coluna a ser atualizada] = [novo valor da coluna], [outra coluna a ser atualizada] = [novo valor da coluna], ...
+-- WHERE [coluna para achar o registro que vc quer] = [valor da coluna do dado que vc quer atualizar]
+
+--#############################################################################################################################
+
+-- Deleting an data
+
+-- DELETE FROM [nome da tabela] WHERE [coluna para achar o registro que quer apagar] = [valor dessa coluna], ...;
+
---
+++ PostgreSQL/udemy-course/annotations/tables.sql
@@ -0,0 +1,40 @@
+-- Criar tabela
+
+-- CREATE TABLE nome_da_tabela (
+--   nome_da_coluna1 [tipo de dado] [constraints],
+--   nome_da_coluna2 [tipo de dado] [constraints],
+--   ...
+-- );
+
+-- onde [constraints] são as contraints para a coluna, como 'primary key', 'not null', 'unique' e etc
+
+--################################################################################
+
+-- Para criar uma 'foreign key', faz na definição da coluna que será a 'foreign key' o seguinte
+
+-- nome_da_coluna [tipo de dado] REFERENCES [nome da tabela a ser referenciada] ([coluna referenciada])
+
+--################################################################################
+
+-- Para adicionar uma 'primary key' composta, na criação da tabela adiciona-se a seguinte linha
+
+-- PRIMARY KEY ([coluna1], [coluna2], ... )
+
+--################################################################################
+
+-- Para alterar uma tabela deve-se usar 'alter table' para então usar as opções de alterações
+
+-- ALTER TABLE [nome da tabela]
+-- [alterações]
+
+-- Alguns exemplos de alteração são:
+-- Adicionar coluna: 
+--    ADD COLUMN [nome da coluna] [constraints]
+-- Modificar tipo de dado da coluna:
+--    ALTER COLUMN [nome da coluna] TYPE [novo tipo de dado]
+
+--################################################################################
+
+-- Para deletar uma tabela faz
+
+-- DROP TABLE [nome da tabela]
