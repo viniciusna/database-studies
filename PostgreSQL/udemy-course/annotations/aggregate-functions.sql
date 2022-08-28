c1cc41218f1a33c81c60307793e6204d4ef2ba02
Vinicius Noronha <viniciusna633@outlook.com>
2022-08-28 15:36:52 -0300

    udemy course starting section 6

---
+++ PostgreSQL/udemy-course/annotations/aggregate-functions.sql
@@ -0,0 +1,4 @@
+-- Aggregate function são funções que agrupam dados de uma coluna e retorna uma linha ou valor, como por exemplo, count, que conta
+-- quantos rows tem a tabela, max, min, sum e average que o nome já indica o que fazem.
+
+-- #########################################################################################################################
